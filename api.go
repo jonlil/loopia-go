af8d0a09b69b74b378100642869c3592e9e91073
Jonas Liljestrand <jonas.liljestrand@gmail.com>
2018-11-02 09:57:15 +0100

    fetch records from Loopia API

--- api.go
+++ api.go
@@ -1,46 +1,49 @@
-package loopia 
+package loopia
 
 import (
     "github.com/kolo/xmlrpc"
     "fmt"
     "errors"
+    "os"
+    "log"
 )
 
 type Record struct {
-    Id int `xmlrpc:"record_id"`
-    Ttt int `xmlrpc:"ttl"`
+    Id int64 `xmlrpc:"record_id"`
+    Ttl int `xmlrpc:"ttl"`
     Type string `xmlrpc:"type"`
     Value string `xmlrpc:"rdata"`
     Priority int `xmlrpc:"priority"`
 }
 
-func GetZoneRecords(b []byte) []Record {
-    resp := xmlrpc.NewResponse(b)
+func NewClient() (*xmlrpc.Client, error) {
+  return xmlrpc.NewClient(API_URL, nil)
+}
+
+func GetZoneRecords(domain string, subdomain string) []Record {
+    client, _ := NewClient()
 
     result := []Record{}
+    args := []interface{}{
+        os.Getenv("LOOPIA_USERNAME"),
+        os.Getenv("LOOPIA_PASSWORD"),
+        "",
+        domain,
+        subdomain,
+    }
 
-    if err := resp.Unmarshal(&result); err != nil {
-        fmt.Println("Error:", err)
+    if err := client.Call("getZoneRecords", args, &result); err != nil {
+      fmt.Println("Error:", err)
     }
 
     return result
 }
 
-type RecordGetter func(domain string, subdomain string, id int) []Record
-
-type GetZoneRecord struct {
-    get_records RecordGetter
-}
-
-func NewGetZoneRecord (rg RecordGetter) *GetZoneRecord {
-    return &GetZoneRecord{get_records: rg}
-}
-
-func (r *GetZoneRecord) getZoneRecord(domain string, subdomain string, id int) (Record, error) {
-    for _, element := range r.get_records(domain, subdomain, id) {
+func GetZoneRecord(domain string, subdomain string, id int64) (Record, error) {
+    for _, element := range GetZoneRecords(domain, subdomain) {
         if id == element.Id {
             return element, nil
         }
     }
-    return Record{}, errors.New("ID Not found") 
+    return Record{}, errors.New("ID Not found")
 }
