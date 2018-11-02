689c1d57a50dd3a50a3ccbe7fb887031f7399f29
Jonas Liljestrand <jonas.liljestrand@gmail.com>
2018-11-03 00:33:58 +0100

    store fixtures inside fixture folder

--- dns_test.go
+++ dns_test.go
@@ -15,7 +15,7 @@ func TestClient_GetZoneRecord(t *testing.T) {
 
 	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		assert.Equal(t, "POST", r.Method, "Expected method 'POST', got %s", r.Method)
-		byteArray, _ := ioutil.ReadFile("response.xml")
+		byteArray, _ := ioutil.ReadFile("fixtures/response.xml")
 		fmt.Fprintf(w, string(byteArray[:]))
 	})
 	record, _ := client.GetZoneRecord("example.com", "@", 14096733)
